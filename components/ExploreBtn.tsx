7ef8b1d51826949156759350d2c2d68bd7846f08
ankush-github-11 <ankush10yt@gmail.com>
2026-02-18 01:46:49 +0530

    Implementing PostHog

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,8 +1,14 @@
 "use client";
 import Image from "next/image";
+import posthog from "posthog-js";
+
 const ExploreBtn = () => {
+  const handleExploreClick = () => {
+    posthog.capture("explore_events_clicked");
+  };
+
   return (
-    <button type="button" id="explore-btn" className="mx-auto mt-7">
+    <button type="button" id="explore-btn" className="mx-auto mt-7" onClick={handleExploreClick}>
         <a href="#events">
             Explore Events
             <Image src="/icons/arrow-down.svg" alt="Arrow Down" width={20} height={20}></Image>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,29 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import posthog from "posthog-js";
 
 const Navbar = () => {
+  const handleNavLinkClick = (linkName: string) => {
+    posthog.capture("nav_link_clicked", {
+      link_name: linkName,
+    });
+  };
+
   return (
     <header>
       <nav>
-        <Link href="/" className="logo">
+        <Link href="/" className="logo" onClick={() => handleNavLinkClick("Logo")}>
           <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
 
           <p>DevEvent</p>
         </Link>
 
         <ul>
-          <Link href="/">Home</Link>
-          <Link href="/">Events</Link>
-          <Link href="/">Create Event</Link>
+          <Link href="/" onClick={() => handleNavLinkClick("Home")}>Home</Link>
+          <Link href="/" onClick={() => handleNavLinkClick("Events")}>Events</Link>
+          <Link href="/" onClick={() => handleNavLinkClick("Create Event")}>Create Event</Link>
         </ul>
       </nav>
     </header>
